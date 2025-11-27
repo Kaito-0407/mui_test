b1b19a5d936e4a1b1606ad46bb68f62a33c28fc8
mashroom5648 <mashroom5648@gmail.com>
2025-11-27 17:33:15 +0900

    fix: deploy check

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ export default function Header({ username = 'ゲスト' }: HeaderProps) {
               }
             }}
           >
-            BOARD
+            BOARDR
           </Typography>
         </Link>
 
